1d52f8097324c213eace250323dd39605c2803b1
Abhilash Murthy <abhilashm.2010@sis.smu.edu.sg>
2013-06-23 20:14:17 +0800

    Changed package names Updated SQL dump

---
+++ src/main/java/userAction/ResponseAction.java
@@ -0,0 +1,13 @@
+// This class is for both accepting/rejecting a booking
+
+package userAction;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ *
+ * @author User
+ */
+public class ResponseAction extends ActionSupport{
+    
+}
--- src/main/resources/SQL/is480-scheduling.sql
+++ src/main/resources/SQL/is480-scheduling.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.1
+-- version 3.4.5
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 16, 2013 at 09:57 AM
--- Server version: 5.5.24-log
--- PHP Version: 5.3.13
+-- Generation Time: Jun 23, 2013 at 12:09 PM
+-- Server version: 5.5.16
+-- PHP Version: 5.3.8
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -26,70 +26,13 @@ SET time_zone = "+00:00";
 -- Table structure for table `schedule`
 --
 
-DROP TABLE IF EXISTS `schedule`;
 CREATE TABLE IF NOT EXISTS `schedule` (
-  `phase` varchar(50) NOT NULL COMMENT 'Refers to Acceptance/Midterm/Final',
+  `milestone` varchar(50) NOT NULL COMMENT 'Refers to Acceptance/Midterm/Final',
   `startDate` date NOT NULL,
   `endDate` date NOT NULL,
-  `yearTerm` varchar(50) NOT NULL,
-  PRIMARY KEY (`phase`,`yearTerm`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `team`
---
-
-CREATE TABLE IF NOT EXISTS `team` (
-  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(100) NOT NULL,
-  `yearTerm` varchar(50) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `time_slot`
---
-
-CREATE TABLE IF NOT EXISTS `time_slot` (
-  `phase` varchar(50) NOT NULL,
-  `yearTerm` varchar(50) NOT NULL,
-  `startDateTime` datetime NOT NULL,
-  `endDateTime` datetime NOT NULL,
-  `teamId` bigint(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`phase`,`yearTerm`,`startDateTime`),
-  KEY `teamId` (`teamId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user`
---
-
-CREATE TABLE IF NOT EXISTS `user` (
-  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
-  `firstName` varchar(50) NOT NULL,
-  `lastName` varchar(50) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `teamId` bigint(10) unsigned DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `teamId` (`teamId`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `user_role`
---
-
-CREATE TABLE IF NOT EXISTS `user_role` (
-  `userId` bigint(10) unsigned NOT NULL,
-  `role` varchar(50) NOT NULL,
-  KEY `userId` (`userId`)
+  `term_id` bigint(10) unsigned NOT NULL,
+  PRIMARY KEY (`milestone`,`term_id`),
+  KEY `term_id` (`term_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -97,23 +40,10 @@ CREATE TABLE IF NOT EXISTS `user_role` (
 --
 
 --
--- Constraints for table `time_slot`
+-- Constraints for table `schedule`
 --
-ALTER TABLE `time_slot`
-  ADD CONSTRAINT `FK_schedule` FOREIGN KEY (`phase`, `yearTerm`) REFERENCES `schedule` (`phase`, `yearTerm`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `time_slot_ibfk_1` FOREIGN KEY (`teamId`) REFERENCES `team` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
---
--- Constraints for table `user`
---
-ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`teamId`) REFERENCES `team` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
-
---
--- Constraints for table `user_role`
---
-ALTER TABLE `user_role`
-  ADD CONSTRAINT `user_role_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE `schedule`
+  ADD CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`term_id`) REFERENCES `term` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
