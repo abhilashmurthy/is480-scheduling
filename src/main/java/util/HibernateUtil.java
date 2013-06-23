0e8a05569983c352022cee9773a9f723e47578a0
Abhilash Murthy <abhilashm.2010@sis.smu.edu.sg>
2013-06-23 22:22:27 +0800

    HibernateUtil updated. Not working :(

--- src/main/java/util/HibernateUtil.java
+++ src/main/java/util/HibernateUtil.java
@@ -5,17 +5,27 @@
 package util;
 
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.AnnotationConfiguration;
 import org.hibernate.cfg.Configuration;
- 
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+
+/**
+ *
+ * @author suresh
+ */
 public class HibernateUtil {
+    
+    private static SessionFactory sessionFactory;
+    private static ServiceRegistry serviceRegistry;
  
-    private static final SessionFactory sessionFactory = buildSessionFactory();
- 
-    private static SessionFactory buildSessionFactory() {
+    static {
         try {
             // Create the SessionFactory from hibernate.cfg.xml
-            return new AnnotationConfiguration().configure().buildSessionFactory();
+            Configuration configuration = new Configuration();
+            sessionFactory = configuration.configure().buildSessionFactory();
+//            serviceRegistry = new ServiceRegistryBuilder()
+//                    .applySettings(configuration.getProperties()).buildServiceRegistry();        
+//            sessionFactory = configuration.buildSessionFactory(serviceRegistry);
         }
         catch (Throwable ex) {
             // Make sure you log the exception, as it might be swallowed
@@ -32,5 +42,5 @@ public class HibernateUtil {
     	// Close caches and connection pools
     	getSessionFactory().close();
     }
- 
+    
 }
