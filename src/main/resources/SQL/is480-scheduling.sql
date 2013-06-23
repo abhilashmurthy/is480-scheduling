7838c157b69d18636b96f0aab2070ad5278239de
Abhilash Murthy <abhilashm.2010@sis.smu.edu.sg>
2013-06-23 20:34:21 +0800

    Updated SQL dump

--- src/main/resources/SQL/is480-scheduling.sql
+++ src/main/resources/SQL/is480-scheduling.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 23, 2013 at 12:09 PM
+-- Generation Time: Jun 23, 2013 at 12:33 PM
 -- Server version: 5.5.16
 -- PHP Version: 5.3.8
 
@@ -35,6 +35,83 @@ CREATE TABLE IF NOT EXISTS `schedule` (
   KEY `term_id` (`term_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `team`
+--
+
+CREATE TABLE IF NOT EXISTS `team` (
+  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `term_id` bigint(10) unsigned NOT NULL,
+  `reviewer_1` bigint(10) unsigned DEFAULT NULL,
+  `reviewer_2` bigint(10) unsigned DEFAULT NULL,
+  `supervisor` bigint(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `term`
+--
+
+CREATE TABLE IF NOT EXISTS `term` (
+  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `year` year(4) DEFAULT NULL,
+  `term` smallint(1) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `time_slot`
+--
+
+CREATE TABLE IF NOT EXISTS `time_slot` (
+  `term_id` bigint(10) unsigned NOT NULL,
+  `startDate` datetime NOT NULL,
+  `endDate` datetime NOT NULL,
+  `team_id` bigint(20) NOT NULL,
+  `milestone` varchar(50) NOT NULL,
+  PRIMARY KEY (`term_id`,`startDate`,`milestone`),
+  UNIQUE KEY `term_id` (`term_id`),
+  UNIQUE KEY `milestone` (`milestone`),
+  KEY `FK_schedule` (`milestone`,`term_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` bigint(10) unsigned NOT NULL AUTO_INCREMENT,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `teamId` bigint(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `teamId` (`teamId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_role`
+--
+
+CREATE TABLE IF NOT EXISTS `user_role` (
+  `userId` bigint(10) unsigned NOT NULL,
+  `term_id` bigint(10) unsigned NOT NULL,
+  `role` varchar(50) NOT NULL,
+  UNIQUE KEY `term_id` (`term_id`),
+  KEY `userId` (`userId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 --
 -- Constraints for dumped tables
 --
@@ -44,6 +121,25 @@ CREATE TABLE IF NOT EXISTS `schedule` (
 --
 ALTER TABLE `schedule`
   ADD CONSTRAINT `schedule_ibfk_1` FOREIGN KEY (`term_id`) REFERENCES `term` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `time_slot`
+--
+ALTER TABLE `time_slot`
+  ADD CONSTRAINT `FK_schedule` FOREIGN KEY (`milestone`, `term_id`) REFERENCES `schedule` (`milestone`, `term_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`teamId`) REFERENCES `team` (`id`) ON DELETE SET NULL ON UPDATE CASCADE;
+
+--
+-- Constraints for table `user_role`
+--
+ALTER TABLE `user_role`
+  ADD CONSTRAINT `user_role_ibfk_2` FOREIGN KEY (`term_id`) REFERENCES `term` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `user_role_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
