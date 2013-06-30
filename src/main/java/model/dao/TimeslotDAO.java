a69e988a0e0aecae933ab942c635b4a37f3909e0
Suresh Subramaniam <suresh.s.2010@sis.smu.edu.sg>
2013-06-30 20:00:56 +0800

    Coded Create Booking use case. Not tested

--- src/main/java/model/dao/TimeslotDAO.java
+++ src/main/java/model/dao/TimeslotDAO.java
@@ -4,10 +4,64 @@
  */
 package model.dao;
 
+import java.math.BigInteger;
+import java.util.Date;
+import model.Timeslot;
+import static model.dao.TimeslotDAO.session;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import util.HibernateUtil;
+
 /**
  *
  * @author suresh
  */
 public class TimeslotDAO {
-	
+	static final Logger logger = LoggerFactory.getLogger(TimeslotDAO.class);
+
+    static Session session = HibernateUtil.getSession();
+
+    public static void save(Timeslot timeslot) {
+        session.beginTransaction();
+        session.save(timeslot);
+        session.getTransaction().commit();
+        logger.info("Added timeslot TermId:" + timeslot.getId().getTermId()
+				+ " Milestone: " + timeslot.getId().getMilestone()
+				+ " Start Time: " + timeslot.getId().getStartTime());
+    }
+
+    public static void update(Timeslot timeslot) {
+        session.beginTransaction();
+        session.update(timeslot);
+        session.getTransaction().commit();
+        logger.info("Updated timeslot TermId:" + timeslot.getId().getTermId()
+				+ " Milestone: " + timeslot.getId().getMilestone()
+				+ " Start Time: " + timeslot.getId().getStartTime());
+    }
+
+    public static void delete(Timeslot timeslot) {
+        session.beginTransaction();
+        session.delete(timeslot);
+        session.getTransaction().commit();
+        logger.info("Delete timeslot TermId:" + timeslot.getId().getTermId()
+				+ " Milestone: " + timeslot.getId().getMilestone()
+				+ " Start Time: " + timeslot.getId().getStartTime());
+    }
+
+    public static Timeslot findByTimeslotId(int termId, String milestone, Date startTime) {
+        session.beginTransaction();
+        BigInteger bigIntTermId = BigInteger.valueOf(termId);
+        Query query = session.createQuery("from Timeslot where "
+				+ "id.termId = :termId and "
+				+ "id.milestone = :milestone "
+				+ "id.startTime = :startTime");
+        query.setParameter("termId", bigIntTermId);
+		query.setParameter("milestone", milestone);
+		query.setParameter("startTime", startTime);
+        Timeslot timeslot = (Timeslot) query.uniqueResult();
+        session.getTransaction().commit();
+        return timeslot;
+    }
 }
--- src/main/java/userAction/CreateBookingAction.java
+++ src/main/java/userAction/CreateBookingAction.java
@@ -5,9 +5,16 @@
 package userAction;
 
 import com.opensymphony.xwork2.ActionSupport;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import org.apache.struts2.dispatcher.mapper.ActionMapping;
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import model.Schedule;
+import model.Timeslot;
+import model.dao.ScheduleDAO;
+import model.dao.TimeslotDAO;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 /**
  *
  * @author Prakhar
@@ -16,13 +23,56 @@ public class CreateBookingAction extends ActionSupport{
     
     private int teamId;
     private String startTime;
-    private String endTime;
     private int termId;
     private String milestone;
-    
+	private String response;
+
+	static final Logger logger = LoggerFactory.getLogger(CreateBookingAction.class);
+	
     @Override
     public String execute() throws Exception {
-            return "success";
+		//Retrieve the corresponding schedule object and its timeslots
+		Schedule schedule = ScheduleDAO.findByScheduleId(termId, milestone);
+		List<Timeslot> timeslots = schedule.getTimeslots();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+		Date bookingTime = sdf.parse(startTime);
+        
+		//Checking if the team already has a booking (pending/confirmed)
+		for (Timeslot t : timeslots) {
+			if (t.getTeamId().intValue() == teamId) {
+				response = "This team already has a booking.";
+				return "fail";
+			}
+		}
+		
+		//Retrieve the corresponding booking slot
+		Timeslot bookingSlot = null;
+		for (Timeslot t : timeslots) {
+			Date tStartTime = t.getId().getStartTime();
+			if (tStartTime.equals(bookingTime)) {
+				bookingSlot = t;
+				break;
+			}
+		}
+		
+		//Check if timeslot has been found
+		if (bookingSlot == null) {
+			logger.error("Invalid details. Timeslot not found.");
+			return ERROR;
+		}
+		
+		//Check if the timeslot is free
+		if (bookingSlot.getTeamId() != null) { //Slot is full
+				response = "This timeslot is already taken.";
+				return "fail";
+		}
+		
+		//All conditions met. Assign timeslot to team
+		BigInteger bigIntTeamId = BigInteger.valueOf(teamId);
+		bookingSlot.setTeamId(bigIntTeamId);
+		TimeslotDAO.save(bookingSlot);
+		
+		return SUCCESS;
     }
 
     public int getTeamId() {
@@ -41,14 +91,6 @@ public class CreateBookingAction extends ActionSupport{
         this.startTime = startTime;
     }
 
-    public String getEndTime() {
-        return endTime;
-    }
-
-    public void setEndTime(String endTime) {
-        this.endTime = endTime;
-    }
-
     public int getTermId() {
         return termId;
     }
@@ -64,4 +106,13 @@ public class CreateBookingAction extends ActionSupport{
     public void setMilestone(String milestone) {
         this.milestone = milestone;
     }
+	
+	public String getResponse() {
+		return response;
+	}
+
+	public void setResponse(String response) {
+		this.response = response;
+	}
+    
 }
