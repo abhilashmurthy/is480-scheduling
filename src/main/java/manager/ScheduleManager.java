7b88463d7ee3cf19fa94d6aa92975e30cdc94d69
Prakhar Agarwal <prakhara.2010@smu.edu.sg>
2013-07-13 20:13:47 +0800

    Resolved conflicts

--- src/main/java/manager/ScheduleManager.java
+++ src/main/java/manager/ScheduleManager.java
@@ -4,6 +4,9 @@
  */
 package manager;
 
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import javax.persistence.Query;
@@ -33,9 +36,46 @@ public class ScheduleManager {
 			result = (Schedule) q.getSingleResult();
 			em.getTransaction().commit();
 		} catch (Exception e) {
-			logger.error("Database Operation Error");
+			logger.error(e.getMessage());
 			em.getTransaction().rollback();
 		}
 		return result;
 	}
+	
+	public static Schedule findActiveByTerm(Term term) {
+		logger.info("Getting active schedule by term");
+		Schedule result = null;
+		try {
+			em.getTransaction().begin();
+			Query q = em.createQuery("SELECT o FROM Schedule o WHERE o.term = :term"
+					+ " AND o.endDate > :today ORDER BY o.startDate", Schedule.class);
+			q.setParameter("term", term);
+			q.setParameter("today", new Timestamp(Calendar.getInstance().getTimeInMillis()));
+			List<Schedule> resultList = (List<Schedule>) q.getResultList();
+			if (!resultList.isEmpty())
+				result = resultList.get(0);
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			em.getTransaction().rollback();
+		}
+		return result;
+	}
+	
+	public static List<Schedule> findByTerm(Term term) {
+		logger.info("Getting schedules by term");
+		List<Schedule> result = null;
+		try {
+			em.getTransaction().begin();
+			Query q = em.createQuery("SELECT o FROM Schedule o WHERE o.term = :term", Schedule.class);
+			q.setParameter("term", term);
+			result = (List<Schedule>) q.getResultList();
+			em.getTransaction().commit();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+			em.getTransaction().rollback();
+		}
+		return result;
+	}
+	
 }
---
+++ src/main/webapp/footer.jsp
@@ -0,0 +1,10 @@
+<!-- JS imports -->
+<!-- jQuery -->
+<script type="text/javascript" src="js/plugins/jquery-2.0.2.js"></script>
+<!-- jQuery UI -->
+<script type="text/javascript" src="js/plugins/jquery-ui-1.10.3.custom.min.js"></script>
+<script type="text/javascript" src="js/plugins/jquery.timepicker.min.js"></script>
+<!-- Bootstrap -->
+<script type="text/javascript" src="js/plugins/bootstrap.js"></script>
+<!-- Knockout --->
+<script type="text/javascript" src="js/plugins/knockout.js"></script>
