db039f7ec27e8c91904286ae462e97c8c8cd50bb
Suresh Subramaniam <suresh.s.2010@sis.smu.edu.sg>
2013-07-13 18:43:32 +0800

    Imported MiscUtil class from main branch

---
+++ src/main/java/util/MiscUtil.java
@@ -0,0 +1,48 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+import java.util.Calendar;
+import manager.ScheduleManager;
+import manager.TermManager;
+import model.Schedule;
+import model.Term;
+import model.dao.ScheduleDAO;
+import model.dao.TermDAO;
+
+/**
+ * Utility class to put miscellaneous code
+ * @author suresh
+ */
+public class MiscUtil {
+	
+	/**
+	 * Get the current/next active term based on today's date
+	 * Cutoff dates:
+	 * 15-May and 20-Dec
+	 */
+	public static Term getActiveTerm() {
+		int year, semester;
+		Calendar now = Calendar.getInstance();
+		if (now.get(Calendar.MONTH) <= Calendar.MAY && now.get(Calendar.DATE) <= 15) {
+			year = now.get(Calendar.YEAR) - 1;
+			semester = 2;
+		} else if ((now.get(Calendar.MONTH) >= Calendar.MAY && now.get(Calendar.DATE) > 15)
+				&& (now.get(Calendar.MONTH) <= Calendar.DECEMBER && now.get(Calendar.DATE) <= 20)) {
+			year = now.get(Calendar.YEAR);
+			semester = 1;
+		} else {
+			year = now.get(Calendar.YEAR);
+			semester = 2;
+		}
+		
+		return TermManager.findByYearAndSemester(year, semester);
+	}
+	
+	public static Schedule getActiveSchedule() {
+		Term activeTerm = getActiveTerm();
+		return ScheduleManager.findByTermAndMilestone(activeTerm, null);
+	}
+}
