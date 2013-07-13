115016a82e366ea51bc61127a85fd08dfc5e1387
Abhilash Murthy <abhilashm.2010@sis.smu.edu.sg>
2013-07-14 01:11:50 +0800

    -AJAX calls working for Create Term --> Create Schedule panel displayed after Term created successfully -- Modified CreateTermAction to have the right JSON outputs (Note: For a JSON output, ALWAYS return SUCCESS) -- Added createTermJson action - Removed EntityManager.close() on TermManager -- Should not be closed because it is a static, one-time-only variable

--- src/main/java/manager/TermManager.java
+++ src/main/java/manager/TermManager.java
@@ -19,41 +19,40 @@ import org.slf4j.LoggerFactory;
  * @author suresh
  */
 public class TermManager {
-	private static EntityManager em = Persistence.createEntityManagerFactory("scheduler").createEntityManager();
-	private static Logger logger = LoggerFactory.getLogger(TermManager.class);
-	
-	public static boolean save (Term term, EntityTransaction transaction) {
-		logger.info("Creating new term");
-		try {
-			transaction = em.getTransaction();
-			transaction.begin();
-			if (term != null) {
-				em.persist(term);
-			}
-			transaction.commit();
-			return true;
-		} catch (PersistenceException ex) {
-			//Rolling back data transactions
-			if (transaction != null && transaction.isActive()){
-				transaction.rollback();
-			}
-			logger.error("Error making database call for Create Term Details");
-			ex.printStackTrace();
-		} catch (Exception e) {
-			logger.error(e.getMessage(), e);
-			e.printStackTrace();
-		} finally {
-			em.close();
-		}
-		return false;
-	}
-	
-	public static List<Term> getAllTerms() {
-		logger.info("Getting all term objects");
-		List<Term> sourceList = null;
-		EntityTransaction transaction = em.getTransaction();
+
+    private static EntityManager em = Persistence.createEntityManagerFactory("scheduler").createEntityManager();
+    private static Logger logger = LoggerFactory.getLogger(TermManager.class);
+
+    public static boolean save(Term term, EntityTransaction transaction) {
+        logger.info("Creating new term");
         try {
-			transaction.begin();
+            transaction = em.getTransaction();
+            transaction.begin();
+            if (term != null) {
+                em.persist(term);
+            }
+            transaction.commit();
+            return true;
+        } catch (PersistenceException ex) {
+            //Rolling back data transactions
+            if (transaction != null && transaction.isActive()) {
+                transaction.rollback();
+            }
+            logger.error("Error making database call for Create Term Details");
+            ex.printStackTrace();
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static List<Term> getAllTerms() {
+        logger.info("Getting all term objects");
+        List<Term> sourceList = null;
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
             Query q = em.createQuery("Select t from Term t");
             sourceList = q.getResultList();
             transaction.commit();
@@ -62,24 +61,24 @@ public class TermManager {
             //em.getTransaction().rollback();
         }
         return sourceList;
-	}
-	
-	public static Term findByYearAndSemester (int year, int semester) {
-		logger.info("Getting term by year and semester");
-		Term result = null;
-		try {
-			em.getTransaction().begin();
-			Query q = em.createNativeQuery("select * from Term where academicYear = :year "
-					+ "and semester = :semester", Term.class);
-			q.setParameter("year", year);
-			q.setParameter("semester", semester);
-			result = (Term) q.getSingleResult();
-			em.getTransaction().commit();	
-		} catch (Exception e) {
-			e.printStackTrace();
-			logger.error("Database Operation Error");
-			em.getTransaction().rollback();
-		}
-		return result;
-	}
+    }
+
+    public static Term findByYearAndSemester(int year, int semester) {
+        logger.info("Getting term by year and semester");
+        Term result = null;
+        try {
+            em.getTransaction().begin();
+            Query q = em.createNativeQuery("select * from Term where academicYear = :year "
+                    + "and semester = :semester", Term.class);
+            q.setParameter("year", year);
+            q.setParameter("semester", semester);
+            result = (Term) q.getSingleResult();
+            em.getTransaction().commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            logger.error("Database Operation Error");
+            em.getTransaction().rollback();
+        }
+        return result;
+    }
 }
