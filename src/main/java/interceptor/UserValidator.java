2291bfe26a39f53903731cb9870b8a5b776b7a8c
Suresh Subramaniam <suresh.s.2010@sis.smu.edu.sg>
2013-07-20 20:18:34 +0800

    Protected all action classes with UserValidator interceptor

---
+++ src/main/java/interceptor/UserValidator.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interceptor;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.Interceptor;
+import java.util.Map;
+import model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author suresh
+ */
+public class UserValidator implements Interceptor{
+	Logger logger = LoggerFactory.getLogger(UserValidator.class);
+
+	public String intercept(ActionInvocation ai) throws Exception {
+		try {
+			Map<String, Object> session = ActionContext.getContext().getSession();
+
+			User user = (User) session.get("user");
+
+			if (user != null) {
+				return ai.invoke();	
+			} else {
+				return "login";
+			}	
+		} catch (NullPointerException e) {
+			return "login";
+		}		
+	}
+
+	public void destroy() {
+		//Do nothing
+	}
+
+	public void init() {
+		//Do nothing
+	}
+	
+}
