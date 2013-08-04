03a2d3a77d619163b6a6456320bd3ded0879d15a
Abhilash Murthy <abhilashm.2010@sis.smu.edu.sg>
2013-08-04 17:25:37 +0800

    Added try-catch blocks for all system actions

--- src/main/java/systemAction/GetTermsAction.java
+++ src/main/java/systemAction/GetTermsAction.java
@@ -6,6 +6,7 @@ package systemAction;
 
 import static com.opensymphony.xwork2.Action.SUCCESS;
 import com.opensymphony.xwork2.ActionSupport;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -13,6 +14,8 @@ import java.util.Map;
 import java.util.Set;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import manager.TermManager;
 import model.Term;
@@ -26,52 +29,66 @@ import util.MiscUtil;
  *
  * @author Prakhar
  */
-public class GetTermsAction extends ActionSupport implements ServletRequestAware{
-	private List<Term> listTerms;
-	private List<HashMap<String, String>> dataList;
-	private HttpServletRequest request;
-	static final Logger logger = LoggerFactory.getLogger(CreateBookingAction.class);
-	
-	@Override
-	public String execute() {
-		EntityManager em = Persistence.createEntityManagerFactory(MiscUtil.PERSISTENCE_UNIT).createEntityManager();
-		
-		//Getting all the term objects
-		listTerms = TermManager.getAllTerms(em);
-		if (listTerms != null) {
-			dataList = new ArrayList<HashMap<String, String>>();
-			HashMap<String, String> map = null;
-			for (Term term: listTerms) {
-				map = new HashMap<String, String>();
-				String semester = term.getSemester();  //e.g. 1
-				int year = term.getAcademicYear();  //e.g. 2013
-				long termId = term.getId();
-				//e.g. 2013-2014
-				String academicYear = Integer.toString(year) + "-" + Integer.toString(year + 1);
-				//e.g. Term 1
-				String termName = academicYear + semester;
-				map.put("termName", termName);
-				map.put("termId", Long.toString(termId));
-				
-				dataList.add(map);
-			}
-		} else {
-			request.setAttribute("error", "You cannot create a schedule right now. Please create a term first!");
+public class GetTermsAction extends ActionSupport implements ServletRequestAware {
+
+    private List<Term> listTerms;
+    private List<HashMap<String, String>> dataList;
+    private HttpServletRequest request;
+    static final Logger logger = LoggerFactory.getLogger(CreateBookingAction.class);
+    private final boolean debugMode = true;
+
+    @Override
+    public String execute() throws ServletException, IOException {
+        try {
+        EntityManager em = Persistence.createEntityManagerFactory(MiscUtil.PERSISTENCE_UNIT).createEntityManager();
+
+        //Getting all the term objects
+        listTerms = TermManager.getAllTerms(em);
+        if (listTerms != null) {
+            dataList = new ArrayList<HashMap<String, String>>();
+            HashMap<String, String> map = null;
+            for (Term term : listTerms) {
+                map = new HashMap<String, String>();
+                String semester = term.getSemester();  //e.g. 1
+                int year = term.getAcademicYear();  //e.g. 2013
+                long termId = term.getId();
+                //e.g. 2013-2014
+                String academicYear = Integer.toString(year) + "-" + Integer.toString(year + 1);
+                //e.g. Term 1
+                String termName = academicYear + semester;
+                map.put("termName", termName);
+                map.put("termId", Long.toString(termId));
+
+                dataList.add(map);
+            }
+        } else {
+            request.setAttribute("error", "You cannot create a schedule right now. Please create a term first!");
             logger.error("User cannot create schedule before creating term.");
             return ERROR;
-		}
-		return SUCCESS;
-	}
+        }
+        } catch (Exception e) {
+            logger.error("Exception caught: " + e.getMessage());
+            if (debugMode) {
+                for (StackTraceElement s : e.getStackTrace()) {
+                    logger.debug(s.toString());
+                }
+            }
+            request.setAttribute("error", "Error with GetTerms: Escalate to developers!");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("error.jsp");
+            dispatcher.forward(request, null);
+        }
+        return SUCCESS;
+    }
 
-	public List<HashMap<String, String>> getDataList() {
-		return dataList;
-	}
-	
-	public void setDataList(ArrayList<HashMap<String, String>> dataList) {
-		this.dataList = dataList;
-	}
-	
-	public void setServletRequest(HttpServletRequest hsr) {
-		request = hsr;
-	}
+    public List<HashMap<String, String>> getDataList() {
+        return dataList;
+    }
+
+    public void setDataList(ArrayList<HashMap<String, String>> dataList) {
+        this.dataList = dataList;
+    }
+
+    public void setServletRequest(HttpServletRequest hsr) {
+        request = hsr;
+    }
 }
--- src/main/java/systemAction/SendEmailAction.java
+++ src/main/java/systemAction/SendEmailAction.java
@@ -5,11 +5,52 @@
 package systemAction;
 
 import com.opensymphony.xwork2.ActionSupport;
+import java.io.IOException;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import manager.MilestoneManager;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author User
  */
-public class SendEmailAction extends ActionSupport{
+public class SendEmailAction extends ActionSupport implements ServletRequestAware{
+    
+    private HttpServletRequest request;
+    private static Logger logger = LoggerFactory.getLogger(MilestoneManager.class);
+    private final boolean debugMode = true;
+    
+    public String execute() throws ServletException, IOException {
+        try {
+            //Code here
+        } catch (Exception e) {
+            logger.error("Exception caught: " + e.getMessage());
+            if (debugMode) {
+                for (StackTraceElement s : e.getStackTrace()) {
+                    logger.debug(s.toString());
+                }
+            }
+            request.setAttribute("error", "Error with GetSchedule: Escalate to developers!");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("error.jsp");
+            dispatcher.forward(request, null);
+        }
+        return SUCCESS;
+    }
+
+    public HttpServletRequest getRequest() {
+        return request;
+    }
+
+    public void setRequest(HttpServletRequest request) {
+        this.request = request;
+    }
+
+    public void setServletRequest(HttpServletRequest hsr) {
+        this.request = hsr;
+    }
     
 }
