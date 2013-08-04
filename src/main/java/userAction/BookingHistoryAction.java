1c95fe876309ba8978cf1bff487b2be4d27a7857
Prakhar Agarwal <prakhara.2010@smu.edu.sg>
2013-08-04 15:39:08 +0700

    Backend for booking history working correctly (BookingHistoryAction.java) Created a function in MiscUtil to get the schedule based on a timeslot

--- src/main/java/userAction/BookingHistoryAction.java
+++ src/main/java/userAction/BookingHistoryAction.java
@@ -101,8 +101,11 @@ public class BookingHistoryAction extends ActionSupport implements ServletReques
 						String venue = timeslot.getVenue();
 						String teamName = timeslot.getTeam().getTeamName();
 						//Getting the schedule based on timeslot (Each timeslot belongs to 1 unique schedule)
-						Schedule schedule = ScheduleManager.findByTimeslot(em, timeslot.getStartTime());
-//						String milestoneName = schedule.getMilestone().getName();
+						Schedule schedule = MiscUtil.getScheduleByTimeslot(em, timeslot);
+						String milestoneName = "";
+						if (schedule != null) {
+							milestoneName = schedule.getMilestone().getName();
+						}
 						String startTime = sdf.format(timeslot.getStartTime());
 						String endTime = sdf.format(timeslot.getEndTime());
 						
@@ -138,7 +141,7 @@ public class BookingHistoryAction extends ActionSupport implements ServletReques
 						}
 						
 						map.put("teamName", teamName);
-//						map.put("milestone", milestoneName);
+						map.put("milestone", milestoneName);
 						map.put("startTime", startTime);
 						map.put("endTime", endTime);
 						map.put("venue", venue);
@@ -188,128 +191,3 @@ public class BookingHistoryAction extends ActionSupport implements ServletReques
 	}
 	
 } //end of class
-			
-	
-			
-			
-//			//If user is student 
-//			if (role.equalsIgnoreCase("Student")) {
-//				Team team = user.getTeam();
-//				//Checking whether the user belongs to a team or not
-//				if (team != null) { 
-//
-//				} else {
-//					json.put("error", true);
-//					json.put("message", "You are not part of any FYP team!");
-//					return SUCCESS;
-//				}
-//			}
-//			}
-//			//Getting the user's roles for the active term
-//			List<Role> userAllRoles = new ArrayList<Role>();
-//			Term activeTerm = MiscUtil.getActiveTerm(em);
-//			List<Role> activeRoles = RoleManager.getAllRolesByTerm(em, activeTerm);
-//			for (Role role: activeRoles) {
-//				List<User> listUsers = role.getUsers();
-//				for (User userObj: listUsers) {
-//					if (user.equals(userObj)) {
-//						userAllRoles.add(role);
-//					}
-//				}
-//			}
-//				
-//		//List<Role> userAllRoles = user.getRoles();   //This is to get all user roles (and not for active semester)
-//		List<Role> supervisorReviewerRoles = new ArrayList<Role>();
-//		for (Role role: userAllRoles) {
-//			if (role.getName().equalsIgnoreCase("Supervisor") || role.getName().equalsIgnoreCase("Reviewer")) {
-//				supervisorReviewerRoles.add(role);
-//			}
-//		}
-//		
-//		//Checking whether the user is a supervisor/reviewer. Only supervisor/reviewer can approve or reject a booking
-//		if (supervisorReviewerRoles.size() > 0) {
-//			//Getting the current schedule based on term id
-//			Schedule schedule = MiscUtil.getActiveSchedule(em);
-//			//Getting the current milestone
-//			//Set<Timeslot> pendingList = null;
-//			List<Timeslot> userTimeslots = new ArrayList<Timeslot>();
-//			if (schedule != null) {
-//				Set<Timeslot> allTimeslots = schedule.getTimeslots();
-//				//Getting the pending timeslots for the particular user
-//				if (allTimeslots != null && allTimeslots.size() > 0) {
-//					//Iterating over the timeslots. Matching the user and checking if the status is pending.
-//					for(Timeslot currentTimeslot: allTimeslots) {
-//						HashMap<User,Status> statusList = currentTimeslot.getStatusList();
-//						Iterator iter = statusList.keySet().iterator();
-//						while (iter.hasNext()) {
-//							User userObj = (User) iter.next();
-//							if (userObj.equals(user)) {
-//								userTimeslots.add(currentTimeslot);
-//							}
-//						}
-//					}
-//				}
-//				
-//				//Putting all the timeslot details for the user in hash map to display it 
-//				//Sorting the timeslots list (Pending first, Approved/Rejected later)
-//				ArrayList<HashMap<String, String>> pendingList = new ArrayList<HashMap<String, String>>();
-//				ArrayList<HashMap<String, String>> approveRejectList = new ArrayList<HashMap<String, String>>();
-//				if (userTimeslots.size() > 0) {
-//					for (Timeslot timeslot: userTimeslots) {
-//						HashMap<String, String> map = new HashMap<String, String>();
-//						SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yyyy HH:mm aa");
-//						venue = timeslot.getVenue();
-//						timeslotId = timeslot.getId();
-//						Team team = timeslot.getTeam();
-//						teamId = team.getId();				
-//						teamName = team.getTeamName();
-//						milestoneName = schedule.getMilestone().getName();
-//						startTime = sdf.format(timeslot.getStartTime());
-//						endTime = sdf.format(timeslot.getEndTime());
-//						myStatus = timeslot.getStatusList().get(user).toString();
-//						//A user can only have 1 role in a team (Supervisor and Reviewer cannot be same for the same team)
-//						if (team.getSupervisor().equals(user)) {
-//							userRole = "Supervisor";
-//						} else if (team.getReviewer1().equals(user)) {
-//							userRole = "Reviewer";
-//						} else if (team.getReviewer2().equals(user)) {
-//							userRole = "Reviewer";
-//						}
-//						
-//						map.put("timeslotId", String.valueOf(timeslotId));
-//						map.put("teamName", teamName);
-//						map.put("milestone", milestoneName);
-//						map.put("userRole", userRole);
-//						map.put("startTime", startTime);
-//						map.put("endTime", endTime);
-//						map.put("venue", venue);
-//						map.put("myStatus", myStatus);
-//						
-//						if (map.get("myStatus").equalsIgnoreCase("Pending")) {
-//							pendingList.add(map);
-//						} else {
-//							approveRejectList.add(map);
-//						}
-//					}
-//					
-//					for (int i = 0; i < (pendingList.size() + approveRejectList.size()); i++) {
-//						if (i < pendingList.size()) {
-//							data.add(pendingList.get(i));
-//						} else {
-//							data.add(approveRejectList.get(i));
-//						}
-//					} //end of loop
-//				}
-//			}
-////			String value = request.getParameter("value");
-////			if (value.equals("1")) {
-////				
-////			}
-//			return SUCCESS;
-//		}
-//        request.setAttribute("error", "Oops. You're not authorized to access this page!");
-//		logger.error("User cannot access this page");
-//		return ERROR;
-//    }
-//	
-//}
