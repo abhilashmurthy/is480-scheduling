b4ed520505e4ecfe3c407154060ee41dc784758d
Abhilash Murthy <abhilashm.2010@sis.smu.edu.sg>
2013-08-05 16:24:54 +0800

    Made Richard the supervisor of the team for demo

--- src/main/java/util/DBInitUtil.java
+++ src/main/java/util/DBInitUtil.java
@@ -186,6 +186,7 @@ public class DBInitUtil {
 		u2.setUsername("abhilashm.2010");
 		u2.setFullName("Abhilash MURTHY");
 		u2.addRole(student);
+                u2.addRole(admin);
 		
 		User u3 = new User();
 		u3.setUsername("tsgill.ps.2010");
@@ -208,6 +209,7 @@ public class DBInitUtil {
 		u6.setUsername("rcdavis");
 		u6.setFullName("Richard C. DAVIS");
 		u6.addRole(supervisor);
+                u6.addRole(reviewer);
 		
 		User u7 = new User();
 		u7.setUsername("yskim");
@@ -236,8 +238,8 @@ public class DBInitUtil {
 		Team t1 = new Team();
 		t1.setTerm(term12013);
 		t1.setTeamName("Thunderbolt");
-		t1.setSupervisor(u5);
-		t1.setReviewer1(u5);
+		t1.setSupervisor(u6);
+		t1.setReviewer1(u6);
 		t1.setReviewer2(u8);
 		HashSet<User> members = new HashSet<User>();
 		members.add(u1);
