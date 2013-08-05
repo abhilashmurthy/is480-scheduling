a1ca47fa6a575b7c954dfc01947b34ea31ff1dc7
Abhilash Murthy <abhilashm.2010@sis.smu.edu.sg>
2013-08-05 16:11:08 +0800

    Made Xuling the supervisor and reviewer1 of Thunderbolt for demo

--- src/main/java/util/DBInitUtil.java
+++ src/main/java/util/DBInitUtil.java
@@ -236,8 +236,8 @@ public class DBInitUtil {
 		Team t1 = new Team();
 		t1.setTerm(term12013);
 		t1.setTeamName("Thunderbolt");
-		t1.setSupervisor(u4);
-		t1.setReviewer1(u4);
+		t1.setSupervisor(u5);
+		t1.setReviewer1(u5);
 		t1.setReviewer2(u8);
 		HashSet<User> members = new HashSet<User>();
 		members.add(u1);
