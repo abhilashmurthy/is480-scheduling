375b94b675682b6f62e4c678002b63e452aad864
Abhilash Murthy <abhilashm.2010@sis.smu.edu.sg>
2013-09-16 23:41:36 +0800

    Email sent when system deletes overdue pending bookings - NOTE: A race condition can exist here between sending the email and deleting the booking. Must lock the timeslot. Removed JobA.java Removed ViewBookingAction (Not used anymore because it's integrated with GetScheduleAction)

--- src/main/java/systemAction/quartz/ClearPendingBookingJob.java
+++ src/main/java/systemAction/quartz/ClearPendingBookingJob.java
@@ -5,15 +5,19 @@
 package systemAction.quartz;
 
 import constant.BookingStatus;
+import constant.Role;
 import java.sql.Timestamp;
 import java.util.Calendar;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.NoResultException;
 import javax.persistence.Query;
+import manager.UserManager;
 import model.Booking;
 import model.CronLog;
 import model.Timeslot;
+import model.User;
+import notification.email.RejectedBookingEmail;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -31,34 +35,44 @@ public class ClearPendingBookingJob implements Job {
     private int noOfDaysToRespond;
 
     public void execute(JobExecutionContext jec) throws JobExecutionException {
-		//Initializing run log to be stored in database
-		CronLog logItem = new CronLog();
-		logItem.setJobName("Clear Pending Bookings");
-		Calendar cal = Calendar.getInstance();
-		Timestamp now = new Timestamp(cal.getTimeInMillis());
-		logItem.setRunTime(now);
-		
+        //Initializing run log to be stored in database
+        CronLog logItem = new CronLog();
+        logItem.setJobName("Clear Pending Bookings");
+        Calendar cal = Calendar.getInstance();
+        Timestamp now = new Timestamp(cal.getTimeInMillis());
+        logItem.setRunTime(now);
+
         EntityManager em = null;
-        noOfDaysToRespond = 1; //TODO: Remove hardcoding
+        noOfDaysToRespond = 2; //TODO: Remove hardcoding
         try {
             em = MiscUtil.getEntityManagerInstance();
+            
             em.getTransaction().begin();
+            
+            User systemAsUser = new User("is480.scheduling", "IS480 Scheduling System", null, null, null);
+                    
             List<Booking> pendingBookings = null;
             Query queryBookings = em.createQuery("select p from Booking p where p.bookingStatus = :pendingBookingStatus")
                     .setParameter("pendingBookingStatus", BookingStatus.PENDING);
             pendingBookings = (List<Booking>) queryBookings.getResultList();
-			
-			if (pendingBookings.isEmpty()) throw new  NoResultException();
-            
-			for (Booking pendingBooking : pendingBookings) {
+
+            if (pendingBookings.isEmpty()) {
+                throw new NoResultException();
+            }
+
+            for (Booking pendingBooking : pendingBookings) {
                 //Do the time calculation
                 cal.clear();
                 cal.setTimeInMillis(pendingBooking.getCreatedAt().getTime());
                 cal.add(Calendar.DATE, noOfDaysToRespond);
-//                    cal.add(Calendar.MINUTE, noOfDaysToRespond); //For testing
+//                cal.add(Calendar.MINUTE, noOfDaysToRespond); //For testing
                 Timestamp dueDate = new Timestamp(cal.getTimeInMillis());
                 //Delete booking is date is passed
                 if (now.after(dueDate)) {
+                    //Send an email to inform that booking has been rejected by system
+                    RejectedBookingEmail rejectedEmail = new RejectedBookingEmail(pendingBooking, systemAsUser);
+                    rejectedEmail.sendEmail();
+                    
                     logger.debug("Booking: " + pendingBooking + " passed due date. Deleting.");
                     pendingBooking.setBookingStatus(BookingStatus.REJECTED);
                     pendingBooking.setLastEditedBy("IS480 Scheduling System");
@@ -68,20 +82,19 @@ public class ClearPendingBookingJob implements Job {
                     ts.setCurrentBooking(null);
                     em.persist(pendingBooking);
                     em.persist(ts);
-                    //TODO: Add email notification for this task
                 }
             }
             em.getTransaction().commit();
-			logItem.setSuccess(true);
-			logItem.setMessage("Pending bookings cleared.");
+            logItem.setSuccess(true);
+            logItem.setMessage("Pending bookings cleared.");
         } catch (NoResultException n) {
             //Normal, no pending bookings found
-			logItem.setSuccess(true);
-			logItem.setMessage("No pending bookings to clear.");
+            logItem.setSuccess(true);
+            logItem.setMessage("No pending bookings to clear.");
             logger.trace("There are no pending bookings now");
         } catch (Exception e) {
-			logItem.setSuccess(false);
-			logItem.setMessage("Error: " + e.getMessage());
+            logItem.setSuccess(false);
+            logItem.setMessage("Error: " + e.getMessage());
             logger.error("Exception caught: " + e.getMessage());
             if (MiscUtil.DEV_MODE) {
                 for (StackTraceElement s : e.getStackTrace()) {
@@ -90,14 +103,20 @@ public class ClearPendingBookingJob implements Job {
             }
         } finally {
             if (em != null) {
-				//Saving job log in database
-				if (!em.getTransaction().isActive()) em.getTransaction().begin();
-				em.persist(logItem);
-				em.getTransaction().commit();
-				
-				if (em.getTransaction().isActive()) em.getTransaction().rollback();
-				if (em.isOpen()) em.close();
-			}
+                //Saving job log in database
+                if (!em.getTransaction().isActive()) {
+                    em.getTransaction().begin();
+                }
+                em.persist(logItem);
+                em.getTransaction().commit();
+
+                if (em.getTransaction().isActive()) {
+                    em.getTransaction().rollback();
+                }
+                if (em.isOpen()) {
+                    em.close();
+                }
+            }
         }
     }
 }
