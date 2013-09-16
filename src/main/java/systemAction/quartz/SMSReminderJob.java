04a3bb4037b089d92a2c36978eeedf164ea914ab
Abhilash Murthy <abhilashm.2010@sis.smu.edu.sg>
2013-09-16 23:10:33 +0800

    Added SMS_USERNAME and SMS_PASSWORD into General Properties Fixed SMS sending code - SMSes now tested and working Changed SMS type to Flash SMS (Appears on screen, but is not stored on phone - We should discuss this later)

--- src/main/java/systemAction/quartz/SMSReminderJob.java
+++ src/main/java/systemAction/quartz/SMSReminderJob.java
@@ -4,9 +4,13 @@
  */
 package systemAction.quartz;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Set;
 import javax.persistence.EntityManager;
@@ -33,79 +37,91 @@ public class SMSReminderJob implements Job {
 
     public void execute(JobExecutionContext jec) throws JobExecutionException {
         logger.debug("Started SMSReminderJob");
-		//Initializing run log to be stored in database
-		CronLog logItem = new CronLog();
-		logItem.setJobName("SMS Reminders");
-		Calendar cal = Calendar.getInstance();
-		Timestamp now = new Timestamp(cal.getTimeInMillis());
-		logItem.setRunTime(now);
-		
+        //Initializing run log to be stored in database
+        CronLog logItem = new CronLog();
+        logItem.setJobName("SMS Reminders");
+        Calendar cal = Calendar.getInstance();
+        Timestamp now = new Timestamp(cal.getTimeInMillis());
+        logItem.setRunTime(now);
+
+        SimpleDateFormat smsDateFormat = new SimpleDateFormat("EEE, dd MMM");
+        SimpleDateFormat smsTimeFormat = new SimpleDateFormat("HH:mm");
+        
         EntityManager em = null;
+        HttpURLConnection connection = null;
         try {
             em = MiscUtil.getEntityManagerInstance();
 
-			JobDataMap dataMap = jec.getJobDetail().getJobDataMap();
-			long bookingId = dataMap.getLong("bookingId");
-			Booking booking = em.find(Booking.class, bookingId);
-			
-			//Block SMSs if system is in Dev Mode
-			if (MiscUtil.DEV_MODE) {
-				logItem.setSuccess(true);
-				logItem.setMessage("Job triggered with Booking ID: " + booking.getId() + ". SMS not sent.");
-				return;
-			}
+            JobDataMap dataMap = jec.getJobDetail().getJobDataMap();
+            long bookingId = dataMap.getLong("bookingId");
+            Booking booking = em.find(Booking.class, bookingId);
 
-			//Check if the booking was found
-			if (booking != null) {
-				logger.debug("Booking: " + booking + ". SMS sending..");
+            //Block SMSs if system is in Dev Mode
+            if (MiscUtil.DEV_MODE) {
+                logItem.setSuccess(true);
+                logItem.setMessage("Job triggered with Booking ID: " + booking.getId() + ". SMS not sent.");
+                return;
+            }
 
-				//go through all required attendees to see if there is a number
-				Set<User> listOfU = booking.getRequiredAttendees();
+            //Check if the booking was found
+            if (booking != null) {
+                logger.debug("Booking: " + booking + ". SMS sending..");
+                StringBuilder msg = new StringBuilder();
+                msg
+                    .append("flashsms:")
+                    .append("From: IS480 Scheduling System. ")
+                    .append("Team ")
+                    .append(booking.getTeam().getTeamName())
+                    .append(" is presenting on ")
+                    .append(smsDateFormat.format(booking.getTimeslot().getStartTime()))
+                    .append(" ")
+                    .append(smsTimeFormat.format(booking.getTimeslot().getStartTime()))
+                    .append(" for FYP ")
+                    .append(booking.getTimeslot().getSchedule().getMilestone().getName())
+                    .append(". See you there!");
 
-				for (User eachU : listOfU) {
+                String countryCode = "65";
+                StringBuilder phoneNums = new StringBuilder();
+                
+                //Get number of each required attendee
+                for (User user : booking.getRequiredAttendees()) {
+                    if (user.getMobileNumber() != null) {
+                        phoneNums.append(countryCode)
+                                    .append(user.getMobileNumber())
+                                    .append(";");
+                    }
+                }
+                
+                //Builds RESTful URL
+                StringBuilder url = new StringBuilder();
+                url.append("http://smsc.vianett.no/v3/send.ashx?")
+                        .append("tel=")
+                        .append(phoneNums.toString().substring(0, phoneNums.length() - 1))
+                        .append("&username=")
+                        .append(MiscUtil.getProperty("General", "SMS_USERNAME").replaceAll("\"", ""))
+                        .append("&msg=")
+                        .append(msg.toString().replaceAll("\\s+", "+"))
+                        .append("&password=")
+                        .append(MiscUtil.getProperty("General", "SMS_PASSWORD").replaceAll("\"", ""));
 
-					//if user has mobile number
-					if(eachU.getMobileNumber()!=null){
+                logger.debug("Sending to URL: " + url.toString());
 
+                //Sends RESTful SMS
+                URL myURL = new URL(url.toString());
+                connection = (HttpURLConnection) myURL.openConnection();
+                connection.connect();
+                if (connection.getResponseCode() != 200) {
+                    throw new Exception("Error response received");
+                }
 
-						String teamName = booking.getTeam().getTeamName();
-						teamName = teamName.replaceAll("\\s+", "+");
-
-						String approvedTimeSlot = booking.getTimeslot().getStartTime().toString();
-						approvedTimeSlot = approvedTimeSlot.replaceAll("\\s+", "+");
-
-						String msg = "Team:+" + teamName
-											+ "is+presenting+on:" + approvedTimeSlot
-												 + ".+See+you+there!+From:IS480+Scheduling+System.";
-
-						String number = "65";
-						number += eachU.getMobileNumber();
-
-						String appendURL = "http://smsc.vianett.no/v3/send.ashx?tel="
-								+ number + "&msg=" + msg + "&username=tsgill.ps.2010@smu.edu.sg&password=h94c3";
-
-						//code for HTTP request to send sms
-						URL myURL = new URL(appendURL);
-						URLConnection myURLConnection = myURL.openConnection();
-						myURLConnection.connect();
-
-						//this code is for HTTP response
-						/*BufferedReader in = new BufferedReader(new InputStreamReader(
-								myURLConnection.getInputStream()));
-						String inputLine;
-						while ((inputLine = in.readLine()) != null) 
-							System.out.println(inputLine);
-						in.close();*/
-					}
-				}
-				logItem.setSuccess(true);
-				logItem.setMessage("SMS Reminders sent for booking ID: " + booking.getId());
-			} else {
-				throw new Exception("Booking not found!");
-			}
+                logItem.setSuccess(true);
+                logItem.setMessage("SMS Reminders sent for booking ID: " + booking.getId());
+            } else {
+                throw new Exception("Booking not found!");
+            }
         } catch (Exception e) {
-			logItem.setSuccess(false);
-			logItem.setMessage("Error: " + e.getMessage());
+            logItem.setSuccess(false);
+            logItem.setMessage("Error: " + e.getMessage());
             logger.error("Exception caught: " + e.getMessage());
             if (MiscUtil.DEV_MODE) {
                 for (StackTraceElement s : e.getStackTrace()) {
@@ -114,14 +130,23 @@ public class SMSReminderJob implements Job {
             }
         } finally {
             if (em != null) {
-				//Saving job log in database
-				if (!em.getTransaction().isActive()) em.getTransaction().begin();
-				em.persist(logItem);
-				em.getTransaction().commit();
-				
-				if (em.getTransaction().isActive()) em.getTransaction().rollback();
-				if (em.isOpen()) em.close();
-			}
+                //Saving job log in database
+                if (!em.getTransaction().isActive()) {
+                    em.getTransaction().begin();
+                }
+                em.persist(logItem);
+                em.getTransaction().commit();
+
+                if (em.getTransaction().isActive()) {
+                    em.getTransaction().rollback();
+                }
+                if (em.isOpen()) {
+                    em.close();
+                }
+            }
+            if (connection != null) {
+                connection.disconnect();
+            }
         }
         logger.debug("Finished SMSReminderJob");
     }
