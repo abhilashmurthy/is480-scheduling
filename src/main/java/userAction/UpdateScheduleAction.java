223fd0c510b3ad3364928c36f63ea11524e27f8b
Suresh Subramaniam <sureshs.592@gmail.com>
2013-09-24 17:32:32 +0800

    UpdateScheduleAction: - Added role checking - Completed term information updating

--- src/main/java/userAction/UpdateScheduleAction.java
+++ src/main/java/userAction/UpdateScheduleAction.java
@@ -6,6 +6,7 @@ package userAction;
 
 import static com.opensymphony.xwork2.Action.SUCCESS;
 import com.opensymphony.xwork2.ActionSupport;
+import constant.Role;
 import java.sql.Timestamp;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -17,19 +18,23 @@ import java.util.List;
 import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.persistence.Query;
 import javax.servlet.http.HttpServletRequest;
 import manager.MilestoneManager;
 import manager.ScheduleManager;
+import manager.TermManager;
 import manager.TimeslotManager;
 import model.Booking;
 import model.Milestone;
 import model.Schedule;
+import model.Term;
 import model.Timeslot;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import static userAction.CreateScheduleAction.logger;
 import util.MiscUtil;
 
 /**
@@ -49,12 +54,33 @@ public class UpdateScheduleAction extends ActionSupport implements ServletReques
             json.put("exception", false);
             em = MiscUtil.getEntityManagerInstance();
 
+			//Checking user role
+			Role activeRole = (Role) request.getSession().getAttribute("activeRole");
+			if (activeRole != Role.ADMINISTRATOR && activeRole != Role.COURSE_COORDINATOR) {
+				logger.error("Unauthorized user");
+				json.put("message", "You do not have the permission to perform this function!");
+				json.put("success", false);
+				return SUCCESS;
+			}
+			
+			//Getting the currently active term
+			Term activeTerm = (Term) request.getSession().getAttribute("currentActiveTerm");
+			
             //Getting input data
             JSONObject inputData = new JSONObject(request.getParameter("jsonData"));
-            JSONArray milestones = inputData.getJSONArray("milestones[]");
+            String newSemesterName = inputData.getString("semester");
+			JSONArray scheduleData = inputData.getJSONArray("schedules");
 
-            //Creating schedule objects for all milestones
+            //Beginning overall transaction
             em.getTransaction().begin();
+			
+			//Updating term name
+			boolean termUpdated = updateTerm(em, activeTerm, newSemesterName);
+			
+			if (!termUpdated) {
+				return SUCCESS;
+			}
+			
             ArrayList<HashMap<String, Object>> scheduleList = new ArrayList<HashMap<String, Object>>();
             for (int i = 0; i < milestones.length(); i++) {
                 HashMap<String, Object> scheduleJson = new HashMap<String, Object>();
@@ -208,6 +234,31 @@ public class UpdateScheduleAction extends ActionSupport implements ServletReques
         }
         return SUCCESS;
     }
+	
+	private boolean updateTerm(EntityManager em, Term activeTerm, String semester) {
+		Query q = em.createQuery("select t from Term t where t.academicYear = :year AND t.semester = :semester AND t NOT IN (:term)")
+				.setParameter("year", activeTerm.getAcademicYear())
+				.setParameter("semester", semester)
+				.setParameter("term", activeTerm);
+		List<Term> terms = q.getResultList();
+		//Checking if the term already exists
+		if (!terms.isEmpty()) {
+			logger.error("Term already exists");
+			json.put("message", "Term already exists");
+			json.put("success", false);
+			return false;
+		}
+		
+		//No conflict. Updating semester details
+		Term updatedTerm = em.find(Term.class, activeTerm.getId());
+		updatedTerm.setSemester(semester);
+		em.persist(updatedTerm);
+		
+		//Refreshing term object in session
+		request.getSession().setAttribute("currentActiveTerm", updatedTerm);
+		
+		return true;
+	}
 
     public HashMap<String, Object> getJson() {
         return json;
