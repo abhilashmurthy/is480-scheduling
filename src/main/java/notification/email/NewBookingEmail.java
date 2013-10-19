4184921c4693fdf0d9c62c3a0552d45804ba3a9b
Prakhar Agarwal <prakhara.2010@smu.edu.sg>
2013-10-20 00:46:54 +0800

    Added due date information to new booking and respond to booking emails. Updating of java template code incomplete!

--- src/main/java/notification/email/NewBookingEmail.java
+++ src/main/java/notification/email/NewBookingEmail.java
@@ -4,12 +4,20 @@
  */
 package notification.email;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
+import javax.persistence.EntityManager;
+import manager.SettingsManager;
 import model.Booking;
+import model.Settings;
 import model.User;
+import util.MiscUtil;
 
 /**
  *
@@ -44,6 +52,31 @@ public class NewBookingEmail extends EmailTemplate{
 		HashMap<String, String> map = new HashMap<String, String>();
 		
 		map = generateStandardDetails(b, map);
+		
+		//Inserting the due date for response
+		EntityManager em = null;
+		try {
+			em = MiscUtil.getEntityManagerInstance();
+			Settings notificationSettings = SettingsManager.getByName(em, "manageNotifications");
+			String jsonData = notificationSettings.getValue();
+			Gson gson = new Gson();
+			
+			JsonArray notifArray = gson.fromJson(jsonData, JsonArray.class);
+			JsonObject clearBookingSetting = notifArray.get(2).getAsJsonObject();
+			String durationStr = clearBookingSetting.get("emailClearFrequency").getAsString();
+			int duration = Integer.parseInt(durationStr);
+			
+			Calendar deadline = Calendar.getInstance();
+			deadline.setTimeInMillis(b.getCreatedAt().getTime());
+			deadline.add(Calendar.DAY_OF_MONTH, duration);
+			deadline.setTimeInMillis(b.getCreatedAt().getTime());
+			
+			SimpleDateFormat sdf = new SimpleDateFormat("EEE, d MMM yyyy HH:mm");
+			map.put("[DUE_DATE]", sdf.format(deadline.getTime()));
+		} finally {
+            if (em != null && em.getTransaction().isActive()) { em.getTransaction().rollback(); }
+            if (em != null && em.isOpen()) { em.close(); }
+        }
 		
 		return map;
 	}
