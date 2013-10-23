a13d7209f16299b5ee4ff694540fd7842cd4546d
Suresh Subramaniam <sureshs.592@gmail.com>
2013-10-23 20:58:03 +0800

    Basic template for ManageUsers setup for Add/Edit/Delete functions.

--- src/main/java/userAction/ManageUsersAction.java
+++ src/main/java/userAction/ManageUsersAction.java
@@ -4,12 +4,13 @@
  */
 package userAction;
 
-import static com.opensymphony.xwork2.Action.ERROR;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import static com.opensymphony.xwork2.Action.SUCCESS;
 import com.opensymphony.xwork2.ActionSupport;
+import constant.Role;
 import java.util.HashMap;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import model.User;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.slf4j.Logger;
@@ -28,6 +29,31 @@ public class ManageUsersAction extends ActionSupport implements ServletRequestAw
 	@Override
 	public String execute() throws Exception {
 		try {
+			User user = (User) request.getSession().getAttribute("user");
+			
+			//Checking if the user is allowed to perform this function
+			if (user.getRole() != Role.ADMINISTRATOR && user.getRole() != Role.COURSE_COORDINATOR) {
+				json.put("success", false);
+				json.put("message", "User does not have access to this page!");
+				return SUCCESS;
+			}
+			
+			//Retrieveing the form data
+			JsonObject dataObj = new Gson().fromJson(request.getParameter("jsonData"), JsonObject.class);
+			String actionType = dataObj.get("action").getAsString(); //Getting the action type being performed
+			
+			//God I wish we had switch-case for Strings (Java 7!)
+			if (actionType.equalsIgnoreCase("add")) { //Add a new user
+				addUser();
+			} else if (actionType.equalsIgnoreCase("edit")) { //Edit an existing user
+				editUser();
+			} else if (actionType.equalsIgnoreCase("delete")) { //Delete an existing user
+				deleteUser();
+			} else {
+				json.put("success", false);
+				json.put("message", "Unknown action. Options: add/edit/delete");
+				return SUCCESS;
+			}
 			
         } catch (Exception e) {
             logger.error("Exception caught: " + e.getMessage());
@@ -41,6 +67,21 @@ public class ManageUsersAction extends ActionSupport implements ServletRequestAw
             json.put("message", "Error with ManageUsers: Escalate to developers!");
         }
         return SUCCESS;
+	}
+	
+	//Method to add a new user to the system
+	public void	addUser() {
+		
+	}
+	
+	//Method to edit an existing user in the system
+	public void	editUser() {
+		
+	}
+	
+	//Method to delete an existing user from the system
+	public void	deleteUser() {
+		
 	}
 
 	public HashMap<String, Object> getJson() {
