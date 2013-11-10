4804dd093f4384acc8efe05d93b1a412fc37c8c1
Suresh Subramaniam <sureshs.592@gmail.com>
2013-11-10 20:51:55 +0800

    Update email template: Sending emails to everyone only if venue and/or time is changed

--- src/main/java/notification/email/EditBookingEmail.java
+++ src/main/java/notification/email/EditBookingEmail.java
@@ -46,15 +46,14 @@ public class EditBookingEmail extends EmailTemplate {
 	@Override
 	public Set<String> generateToAddressList() {
 		Set<String> emails = new HashSet<String>();
-		EntityManager em = null;
-		try {
-			em = MiscUtil.getEntityManagerInstance();
 		
-			//Adding the team members
-			for (User u : b.getTeam().getMembers()) {
-				emails.add(u.getUsername() + "@smu.edu.sg");
-			}
+		//Adding the team members
+		for (User u : b.getTeam().getMembers()) {
+			emails.add(u.getUsername() + "@smu.edu.sg");
+		}
 
+		//Including other people in the email only if it is an important update: VENUE and/or TIME
+		if (partsEdited.contains(EditType.TIME) || partsEdited.contains(EditType.VENUE)) {
 			//Adding required attendees
 			for (User u : b.getResponseList().keySet()) {
 				emails.add(u.getUsername() + "@smu.edu.sg");
@@ -67,18 +66,23 @@ public class EditBookingEmail extends EmailTemplate {
 					break;
 				}
 			}
+
 			//Adding the course coordinator and optional attendees if this is a previously confirmed booking
 			if (confirmed) {
-				//Adding the course coordinator
-				emails.add(UserManager.getCourseCoordinator(em).getUsername() + "@smu.edu.sg");
+				EntityManager em = null;
+				try {
+					em = MiscUtil.getEntityManagerInstance();
+					//Adding the course coordinator
+					emails.add(UserManager.getCourseCoordinator(em).getUsername() + "@smu.edu.sg");
 
-				//Adding the optional attendees
-				for (String s : b.getOptionalAttendees()) {
-					emails.add(s);
+					//Adding the optional attendees
+					for (String s : b.getOptionalAttendees()) {
+						emails.add(s);
+					}
+				} finally {
+					if (em != null && em.isOpen()) em.close();
 				}
-			}
-		} finally {
-			if (em != null && em.isOpen()) em.close();
+			}	
 		}
 		return emails;
 	}
