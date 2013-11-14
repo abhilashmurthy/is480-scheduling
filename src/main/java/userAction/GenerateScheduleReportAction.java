11810c42acdf03cced23955b4f3ca58ea7a698d9
Tarlochan <Tarlochan@10.124.128.104>
2013-11-15 03:28:45 +0800

    Fixed code to generate report dynamically again.

--- src/main/java/userAction/GenerateScheduleReportAction.java
+++ src/main/java/userAction/GenerateScheduleReportAction.java
@@ -87,10 +87,14 @@ public class GenerateScheduleReportAction extends ActionSupport implements Servl
 				long termId = Long.parseLong(reportData.getString("termId"));
 				String milestone = reportData.getString("milestoneName");
 				
+				//termId = 3;
+				//milestone = "Final";
+				
 				boolean termMilestoneMismatch = true;
 				//check if this term and milestone belong together
 				Term thisTerm = TermManager.findTermById(em, termId);
 				List<Milestone> milestones = MilestoneManager.findByTerm(em, thisTerm);
+				
 
 				//for every milestone for the term
 				for (Milestone m : milestones) {
@@ -119,9 +123,18 @@ public class GenerateScheduleReportAction extends ActionSupport implements Servl
 				String[] firstRow = {"Start Date & Time", "End Date & Time", "Venue", "TA", "Current Booking Status",
 					"Required Attendees", "Optional Attendees", "Team", "Team Members", "Last Edited By"};
 				writer.writeNext(firstRow);
-
+				
+				//get the milestone first
+				Milestone milestoneSelected = MilestoneManager.findByNameAndTerm(em,milestone,thisTerm);
+				
+				//get the schedule from that milestone
+				Schedule s = ScheduleManager.findByMilestone(em, milestoneSelected);
+				
+				//get all slots for this schedule
+				Set<Timeslot> allSlots = s.getTimeslots();
+				
 				//get schedules for this term
-				List<Schedule> thisSchedules = ScheduleManager.getAllSchedules(em);
+				/*List<Schedule> thisSchedules = ScheduleManager.getAllSchedules(em);
 				Schedule thisSchedule = new Schedule();
 
 				for (Schedule sch : thisSchedules) {
@@ -131,7 +144,7 @@ public class GenerateScheduleReportAction extends ActionSupport implements Servl
 					}
 				}
 				//get the timeslots for this schedule
-				List<Timeslot> allSlots = TimeslotManager.findBySchedule(em, thisSchedule);
+				List<Timeslot> allSlots = TimeslotManager.findBySchedule(em, thisSchedule);*/
 
 				//for each timeslot, get the information for each column
 				for (Timeslot t : allSlots) {
@@ -186,15 +199,50 @@ public class GenerateScheduleReportAction extends ActionSupport implements Servl
 						Set<User> allUsers = t.getCurrentBooking().getRequiredAttendees();
 						String toAddRequired = "";
 						String toAddStudents = "";
+						
+						int counter = 0;
+						
+						int facultyCounter = 0;
+						
+						for (User u: allUsers){
 
-						for (User u : allUsers) {
+							if (u.getRole() == Role.STUDENT) {
+								
+								if(counter!=allUsers.size()-1){
+									toAddStudents += u.getFullName() + ",";
+								}else{
+									toAddStudents += u.getFullName();
+								}
+								
+							}else{
+								
+								facultyCounter++;
+								
+							}
+							
+							counter++;
+						}
+						
+						counter = 0;
+						
+						for (User u: allUsers){
+							
+							counter++;
 
 							if (u.getRole() == Role.FACULTY) {
-								toAddRequired += u.getFullName() + " ";
-							} else if (u.getRole() == Role.STUDENT) {
-								toAddStudents += u.getFullName() + " ";
+								
+								if(counter!=facultyCounter){
+									toAddRequired += u.getFullName() + ",";
+								}else{
+									toAddRequired += u.getFullName() + ",";
+								}
+								
 							}
+							
+							
 						}
+						
+						
 
 						requiredAttendees = toAddRequired;
 
@@ -206,10 +254,19 @@ public class GenerateScheduleReportAction extends ActionSupport implements Servl
 						HashSet<String> allOptional = t.getCurrentBooking().getOptionalAttendees();
 
 						String optional = "";
+						
+						counter = 0;
 
 						if (allOptional != null) {
 							for (String eachOptional : allOptional) {
-								optional += eachOptional + " ";
+								
+								if(counter!=allOptional.size()-1){
+									optional += eachOptional + ",";
+								}else{
+									optional += eachOptional;
+								}
+								
+								counter++;
 							}
 
 							if (optional.length() > 0) {
